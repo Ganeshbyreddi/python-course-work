e90dfa7b894c6fdd54ec30db604cf8631fd486fd
Ganeshbyreddi <byreddiganesh768@gmail.com>
2026-07-23 08:09:45 +0530

    Add files via upload

---
+++ Day-1/features of python.py
@@ -0,0 +1,14 @@
+Feature of python:-
+
+1.simple and esay to learn
+2.interpreted language
+3.object-oriented
+4.platform independent
+5.free an d open source
+6.large standard library
+7.dynamically typed
+8.portable
+9.extensible
+10.supports multiple programming paradigms
+11.automatic memory management
+12.rich third-party libraries
