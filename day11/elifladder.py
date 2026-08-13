43e5288225db294f0daaec5172b23908f27c3a5e
Ganesh Byreddi <byreddiganesh768@gmail.com>
2026-08-13 16:13:55 +0530

    Add files via upload

---
+++ day11/elifladder.py
@@ -0,0 +1,23 @@
+budget = int(input("Enter the budget:"))
+if budget > 10000:
+    print("Trip")
+ elif budget > 5000:
+    print("Resort Stay")
+ elif budget > 3000:
+    print("Movie and Dinner")
+ elif budget > 1000:
+    print("Cafe and Shopping")
+  elif budget > 500:
+     print("Street Food and Park Visit")
+ else:
+     print("Stay Home")
+budget = int(input())
+if budget > 10000:
+   print("Cloud Hosting")
+   elif budget > 5000:
+      print("Business Hosting")
+      elif budget > 2000:
+         print("Premium Hosting")
+         else:
+            print("Single Hosting")
+            
---
+++ day11/ifelse.py
@@ -0,0 +1,22 @@
+username = input("Username:")
+password = input("Password:")
+
+if username == 'admin' and password == 'admin123':
+    print("Login Successful")
+    else:
+print("Invalid Credentials")
+
+        products = ['Laptop','Mobile','watch']
+        search = input("Search products:")
+        if search in products:
+            print("Product found")
+        else:
+          print("Product not found")
+                
+
+    bill = int(input("Enter the bill:"))
+    if bill>99:
+                    print(bill)
+    else:
+      print(bill+30)
+                    
