51b58a49998f2b761842844dc77eddb29dedbcdc
Ganesh Byreddi <byreddiganesh768@gmail.com>
2026-08-19 20:51:21 +0530

    Add files via upload

---
+++ Day-25/builtin.py
@@ -0,0 +1,121 @@
+'''import sys
+#print(sys.path)
+#print(sys.version)
+print("start")
+sys.exit()
+print("end")'''
+
+
+'''import platform
+print(platform.system())
+print(platform.release())
+print(platform.processor())'''
+
+
+
+'''import math
+print(math.pi)
+print(math.e)
+
+print(math.sqrt(625))
+print(math.pow(2,3))
+
+print(math.ceil(12.00001))
+print(math.ceil(12.3))
+print(math.ceil(12.6))
+print(math.ceil(12.999999999))
+
+print(math.floor(12.00001))
+print(math.floor(12.3))
+print(math.floor(12.6))
+print(math.floor(12.999999999))'''
+
+
+'''import math
+print(math.fabs(-10))
+print(math.factorial(5))
+print(math.gcd(8,24))
+print(math.log(2,2))
+print(math.sin(30))
+print(math.cos(30))
+print(math.tan(30))
+print(math.degrees(30))
+print(math.radians(30))'''
+
+
+'''import random 
+#random.seed(10)
+print(random.randint(1,10))
+print(random.randint(100000,999999))
+print(random.random())
+print(random.uniform(1,6))
+l = ['R','P','S']
+print(random.choice(l))
+print(random.choices(l,k=2))
+random.shuffle(l)
+print(l)'''
+
+
+'''from collections import Counter
+
+s = 'python programming'
+m = 'this is that that is this is is'.split()
+l = [1,2,5,9,3,87,7,65,89,4,5,8,8972,789,34,56,76,]
+
+print(Counter(s))
+print(Counter(l))
+print(Counter(m))'''
+
+
+'''from collections import Counter,defaultdict
+
+s = 'python programming'
+m = 'this is that that is this is is'.split()
+l = [1,2,5,9,3,87,7,65,89,4,5,8,8972,789,34,56,76,]
+
+d = defaultdict(int)
+for i in s:
+    d[i]+=1
+
+print(d)
+'''
+
+'''from collections import Counter,deque
+l = deque([])
+l.append(10)
+l.append(20)
+l.append(30)
+l.popleft()
+l.popleft()
+l.append(50)
+l.append(70)
+l.popleft()
+print(l)
+
+l = deque([])
+l.appendleft(10)
+l.appendleft(20)
+l.appendleft(30)
+l.pop()
+l.pop()
+l.appendleft(50)
+l.appendleft(70)
+l.pop()
+print(l)'''
+
+
+
+'''from itertools import combinations,permutations
+
+res1 = list(combinations('abc',2))
+res2 = list(permutations('abc',2))
+
+['ab','bc','ca']
+print(res1)
+print(res2)
+
+
+
+print([''.join(i) for i in res1])
+print([''.join(i) for i in res2])
+'''
