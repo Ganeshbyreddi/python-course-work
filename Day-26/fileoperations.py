911e9ed5de5a70f64a9aa7c3d0b8b259d6e79f91
Ganesh Byreddi <byreddiganesh768@gmail.com>
2026-08-20 23:47:08 +0530

    Add files via upload

---
+++ Day-26/fileoperations.py
@@ -0,0 +1,23 @@
+'''file = open('pfs-63.txt','r')
+print(file.read())
+file.seek(0)
+print(file.readline())
+file.seek(0)
+print(file.readlines())
+file.close()
+
+
+with open('pfs-63.txt','r') as file:
+    print(file.read())
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())
+
+
+
+with open ('pfs-63.txt','a+') as file:
+    file.write("Tom same branch 5")
+    file.seek(0)
+    print(file.read())
+    '''
