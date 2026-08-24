c209fdbf6cfecf9f517095f2fd2119ff17f7598f
Ganesh Byreddi <byreddiganesh768@gmail.com>
2026-08-24 20:31:55 +0530

    Delete Day-27 directory

--- Day-27/studentinfo.py
+++
@@ -1,7 +0,0 @@
-import json
-with open("data.json",'r') as file:
-     data = json.load(file)
-data["username"] = "viswa"
-data["skills"].append("flask")
-with open("data.json",'w') as file:
-    json.dump(data,file,indent=4)
